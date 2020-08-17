96df986e768a77a5d8e6d038c558603582e3304f
Wrench <65120517+JyothisJayanth@users.noreply.github.com>
2020-08-17 14:39:16 +0530

    test fix

--- m3u8/plugins/youtube_dl_echo.py
+++ m3u8/plugins/youtube_dl_echo.py
@@ -198,32 +198,30 @@ async def echo(bot, update: Message):
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
-        reply_markup = InlineKeyboardMarkup(inline_keyboard)
+        reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
         # logger.info(reply_markup)
-        thumbnail = DEF_THUMB_NAIL_VID_S
-        thumbnail_image = DEF_THUMB_NAIL_VID_S
+        thumbnail = Config.DEF_THUMB_NAIL_VID_S
+        thumbnail_image = Config.DEF_THUMB_NAIL_VID_S
         if "thumbnail" in response_json:
             if response_json["thumbnail"] is not None:
                 thumbnail = response_json["thumbnail"]
                 thumbnail_image = response_json["thumbnail"]
         thumb_image_path = DownLoadFile(
             thumbnail_image,
-            DOWNLOAD_LOCATION + "/" +
+            Config.DOWNLOAD_LOCATION + "/" +
             str(update.from_user.id) + ".jpg",
-            128,
+            Config.CHUNK_SIZE,
             None,  # bot,
             Translation.DOWNLOAD_START,
             update.message_id,
             update.chat.id
         )
-        await update.reply_photo(
-            photo=thumb_image_path,
-            quote=True,
-            caption=Translation.FORMAT_SELECTION.format(
-                thumbnail
-            ) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
             reply_markup=reply_markup,
-            parse_mode="html"
+            parse_mode="html",
+            reply_to_message_id=update.message_id
         )
     else:
         # fallback for nonnumeric port a.k.a seedbox.io
